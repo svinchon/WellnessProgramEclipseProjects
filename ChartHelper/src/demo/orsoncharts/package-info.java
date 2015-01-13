c29c47b11cd6dfc55f5294940aedbc6ae8973db1
svinchon <svinchon@gmail.com>
2015-01-13 22:55:45 +0100

    not sure

--- ChartHelper/src/demo/orsoncharts/package-info.java
+++
@@ -1,9 +0,0 @@
-package demo.orsoncharts;
-
-interface package_info {}
-
-
-/* Location:           C:\Users\vinchs\Desktop\temp\
- * Qualified Name:     demo.orsoncharts.package-info
- * JD-Core Version:    0.7.0.1
- */

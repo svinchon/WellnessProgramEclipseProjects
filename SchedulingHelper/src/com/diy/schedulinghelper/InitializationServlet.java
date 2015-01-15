de3fcd94992ab2a1165573596e9eb45830a14dbe
svinchon <svinchon@gmail.com>
2015-01-15 20:04:36 +0100

    new test aftrer adj

--- SchedulingHelper/src/com/diy/schedulinghelper/InitializationServlet.java
+++ SchedulingHelper/src/com/diy/schedulinghelper/InitializationServlet.java
@@ -22,11 +22,9 @@ public class InitializationServlet extends HttpServlet {
      }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 	
 	void Log(String strMessage) {

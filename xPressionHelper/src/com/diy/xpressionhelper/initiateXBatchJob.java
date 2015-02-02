b5bf49b2cc073c5c2ad023782e3b2613388538a5
svinchon <svinchon@gmail.com>
2015-02-02 17:49:04 +0100

    progressing in data extraction for batch

--- xPressionHelper/src/com/diy/xpressionhelper/initiateXBatchJob.java
+++ xPressionHelper/src/com/diy/xpressionhelper/initiateXBatchJob.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.Locale;
 
 import javax.servlet.ServletException;
@@ -35,30 +36,54 @@ public class initiateXBatchJob extends HttpServlet {
 		String resp = "";
 		String JobName = request.getParameter("JobName");
 		String DataOrigin = request.getParameter("DataOrigin"); //xquery or file
-		String XQuery = request.getParameter("XQuery");;
-		String DataFileName = request.getParameter("DataFileName");;
+		//String XQuery = request.getParameter("XQuery");;
+		//String DataFileName = request.getParameter("DataFileName");;
 		String ts = generateTimeStamp();
 		if (JobName != null && DataOrigin != null) {
+			String metadata = "<metadata>";
+			Enumeration<String> parameterNames = request.getParameterNames();
+			while (parameterNames.hasMoreElements()) {
+				String paramName = parameterNames.nextElement();
+				String paramValue = request.getParameter(paramName);
+				metadata += "<"+paramName+">"+paramValue+"</"+paramName+">";
+			}
+			metadata += "<"+"file_name"+">"+"</"+"file_name"+">";
+			metadata += "<"+"request_type"+">"+"submitForRealTime"+"</"+"request_type"+">";
+			metadata += "</metadata>";
+			XDBHelperProxy xdbhp = new XDBHelperProxy();
 			if (DataOrigin.equals("file")) {
 			} else if (DataOrigin.equals("xquery")) {
-				String strXQ =""
-						+ "<files>"
-						+ "{"
+				String strXQ;
+				strXQ = ""
+						+ "<files> "
+						+ "{ "
 						+ "for $e in doc('AuditTrail.xml')/audit_trail/event "
 						+ "where $e/type = \"submitForBatch\" and not ($e[processed]) "
 						+ "return $e/metadata/file_name "
-						+ "}"
-						+ "</files>";
-				XDBHelperProxy xdbhp = new XDBHelperProxy();
+						+ "} "
+						+ "</files> ";
+				strXQ = ""
+						+ "<users> "
+						+ "{ "
+						+ "for $e in doc('AuditTrail.xml')/audit_trail/event "
+						+ "where $e/type = \"submitForBatch\" and not ($e[processed]) "
+						+ "return doc($e/metadata/file_name/text())/users/* "
+						+ "} "
+						+ "</users>";
 				String strXML = xdbhp.runXQuery(strXQ);
 				String2File(strXML, "C:/tmp/"+"ExtractedForBatch"+ts+".xml");
+				resp = "data extracted";
 			} else {
 				resp = "wrong value for DataOrigin";
 			}
+			String strXQ =""
+					+ "let $e:=<event><time>"+ts+"</time><type>initiateXBatchJob</type>"+metadata+"</event> "
+					+ "return insert node $e as first into /audit_trail";
+			xdbhp.runXQuery(strXQ);
 		} else {
 			resp = "missing input parameters";
 		}
-		xUtilsWSAPI xu = new xUtilsWSAPI();
+		//xUtilsWSAPI xu = new xUtilsWSAPI();
 		//xu.strStartJob(JobName, DataFileName);
 		resp = "{ \"message\": \""+resp+"\" }";
 		response.getWriter().write(resp);

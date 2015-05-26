4d0693b3ee353198810fe84f7e56bfac4bdff530
svinchon <svinchon@gmail.com>
2015-05-26 14:42:49 +0200

    xWord exit and Quick Doc custo

---
+++ xCustomize/src/com/diy/xPressoExits/PickList2.java
@@ -0,0 +1,17 @@
+package com.diy.xPressoExits;
+
+public class PickList2 {
+
+	public static void main(String[] args) {
+		PickList2 p = new PickList2();
+		System.out.println(p.getItem(""));
+	}
+	
+	public PickList2() {
+	}
+	
+	public String getItem(String labelCode) {
+		return "YO!";
+	}
+
+}
